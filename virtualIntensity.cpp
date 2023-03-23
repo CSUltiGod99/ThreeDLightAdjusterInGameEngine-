8dc1b5a9637af8ae120c3d24173d507fba8f0c4f
CSUltiGod99 <80710483+CSUltiGod99@users.noreply.github.com>
2023-03-23 03:01:26 +0000

    Add files via upload

---
+++ virtualIntensity.cpp
@@ -0,0 +1,39 @@
+// C++ - Create a Virtual 3D Light Intensity Method On API:  
+
+
+// source code:
+#include <iostream>
+#include <math>
+
+using namespace lIntensity; // Light Intensity Namespace 
+
+	int intensity()
+	{
+
+
+			for(int f = 0;)
+			{
+				cout << f++ << endl;
+			}
+
+		cout << 0 + 1 << 0 + 2;
+		for 
+
+
+
+	}
+
+
+
+
+
+         
+
+            
+
+            
+
+
+
+
+
